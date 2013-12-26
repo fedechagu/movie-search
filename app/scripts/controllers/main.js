7f926921ee434fefb857851a52b009840d06eb49
Federico Chapa <federico.chapa@provenmethod.com>
2013-12-26 14:07:52 -0500

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('movieApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  })
+.controller('SearchController', function ($scope) {
+
+	
+});
+
