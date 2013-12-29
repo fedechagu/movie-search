3a7b38ec2d25a70aea0142f7985d9f3eb8840d44
Federico Chapa <federico.chapa@provenmethod.com>
2013-12-29 14:21:17 -0500

    basic http setup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,33 @@
 'use strict';
 
 angular.module('movieApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  })
-.controller('SearchController', function ($scope) {
+.controller('MainCtrl', function ($scope) {
+	$scope.awesomeThings = [
+	'HTML5 Boilerplate',
+	'AngularJS',
+	'Karma'
+	];
+})
+.controller('SearchController', function ($scope, $http) {
+	$scope.model = {};
 
 	
+	var baseUrl = 'http://mymovieapi.com/';
+
+	var url = baseUrl + 'type=jsonp&plot=full&episode=1&limit=10&yg=0&mt=none&lang=en-US';
+
+	
+
+	$scope.$watch('model.query', function (newVal, oldVal) {
+		$http({method: 'GET', url: url + '?title=' + newVal}).
+		success(function(data, status) {
+			$scope.model.movieList = data;
+			
+		}).
+		error(function(data, status) {
+
+		});
+	});
+
 });
 
